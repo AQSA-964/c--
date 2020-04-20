0e04290f88fa187278aa000d7cfc9adae9124c0a
AQSA-964 <64035828+AQSA-964@users.noreply.github.com>
2020-04-21 02:50:13 +0500

    Check no. is even or odd
    Write a program that take inputs from user in form of number and outputs whether number is even or odd.

---
+++ 10.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std ;
+
+int main()
+{
+	int m;
+	cout <<"WELCOME!\n" << "WE CAN ENTER THE NUMBER AND CHECK IT'S EVEN OR ODD.\n";
+	cout <<" \nEnter a Number = " ;
+	cin >>m ;
+	
+	if(m%2==0)
+	
+	     cout << " \nEVEN NUMBER ";
+	     
+    else
+    
+         cout << "\nODD NUMBER ";
+         
+         return 0;
+         
+}
