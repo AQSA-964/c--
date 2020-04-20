5d1acd1b5bb3a9625c95a528826a841382225a57
AQSA-964 <64035828+AQSA-964@users.noreply.github.com>
2020-04-21 02:44:14 +0500

    check no. is +ive or -ive
    Write a program that take inputs from user in a form of number and output whether  number is positive or negative.

---
+++ 9.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	int n ;
+	cout <<" WELCOME!\n" <<" WE CAN ENTER THE NUMBER AND CHECK IT'S NEGATIVE OR POSITIVE. ";
+	cout << "\n Enter  Number =" ;
+	cin >> n;
+	
+		if(n>0)
+	    cout <<"NUMBER IS POSITIVE";
+	    
+	else
+	    cout <<"NUMBER IS NEGATIVE";
+	    
+	    return 0;
+	    
+}
