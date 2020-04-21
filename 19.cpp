63a447af040768f84c55b1a796c6e6a71b44e8cd
AQSA-964 <64035828+AQSA-964@users.noreply.github.com>
2020-04-21 15:41:38 +0500

    Table of number
    Write a program to print the multiplication table of a number entered from the keyboard.

---
+++ 19.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int tab, n;
+	
+	cout <<"Enter a number for Table"<<endl;
+	cin >> tab;
+	
+	for (n=1;n<=10;++n)
+	
+	{
+	      cout <<tab <<"*"<<n<<"="<<tab*n <<endl ;
+	
+	}
+	return 0;
+}
